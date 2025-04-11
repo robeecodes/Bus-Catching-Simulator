e262d0a7a940e4ea8a21e79d3337589278bad9de
Robin <97387960+robeecodes@users.noreply.github.com>
2025-04-11 16:47:19 +0100

    feat: implementation of basic dialogue

---
+++ Assets/_Scripts/Singleton.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Instance { get; private set; }
+
+    protected virtual void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
